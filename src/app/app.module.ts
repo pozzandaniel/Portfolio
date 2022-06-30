ffd88589477d2c5ce270798c0b7ca25e43c32785
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-06-30 16:08:47 +0200

    started skillsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeadSectionComponent } from './head-section/head-section.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HeadSectionComponent
+    HeadSectionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
